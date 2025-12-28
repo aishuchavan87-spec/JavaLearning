f2795c228dc49dc0caa7165c539bf3153055f7d7
aishuchavan87-spec <aishuchavan87@gmail.com>
2025-12-28 16:55:51 +0530

    assignmentSwap2values

--- src/main/java/assigmentSwapValues.java
+++ src/main/java/assigmentSwapValues.java
@@ -1,16 +1,31 @@
 public class assigmentSwapValues {
     public static void main(String[] args) {
-        int i = 10;
-        int j =  7;
+//        int i = 10;
+//        int j =  7;
+//
+//
+//        i = i ^ j;
+//        j = i ^ j;
+//        i = i ^ j;
+//
+//        System.out.println(i);
+//        System.out.println(j);
 
 
-        i = i ^ j;
-        j = i ^ j;
-        i = i ^ j;
 
-        System.out.println(i);
-        System.out.println(j);
+
+        int a = 10;
+        int b = 7;
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
 
     }
+
+
 
 }
